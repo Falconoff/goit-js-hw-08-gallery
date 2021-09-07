1e30b4ee26ea811639d34e6c73ce396cd8d1da08
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-09-07 17:08:41 +0300

    add close modal by Esc button or overlay

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ import galleryItems from '../app.js';
 
 const galleryEl = document.querySelector('.js-gallery');
 const lightbox = document.querySelector('.js-lightbox');
-const closeModalBtn = document.querySelector(
+const closeLightboxBtn = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
+const lightboxImage = document.querySelector('.lightbox__image');
 
 // --- create gallery ---
 const galleryItemsArray = galleryItems.map(
@@ -30,7 +31,7 @@ const galleryItemsArray = galleryItems.map(
 
 galleryEl.insertAdjacentHTML('beforeend', galleryItemsArray.join(''));
 
-// --- click on picture ---
+// --- click on picture, open Lightbox ---
 galleryEl.addEventListener('click', e => {
   e.preventDefault();
   // console.log(e.target);
@@ -41,13 +42,43 @@ galleryEl.addEventListener('click', e => {
 
   lightbox.classList.add('is-open');
 
-  const lightboxImage = document.querySelector('.lightbox__image');
+  // const lightboxImage = document.querySelector('.lightbox__image');
   lightboxImage.setAttribute('src', e.target.dataset.source);
   // console.log(e.target.getAttribute('alt'));
   lightboxImage.setAttribute('alt', e.target.getAttribute('alt'));
+
+  window.addEventListener('keydown', onEscPress);
 });
 
-// --- close lightbox ---
-closeModalBtn.addEventListener('click', () => {
-  lightbox.classList.remove('is-open');
+// --- click Close Lightbox Button
+closeLightboxBtn.addEventListener('click', () => {
+  console.log('click on closeLightboxBtn');
+  closeLightbox();
 });
+
+// --- close Lightbox function ---
+function closeLightbox() {
+  console.log('CLOSE');
+  lightbox.classList.remove('is-open');
+
+  lightboxImage.setAttribute('src', '');
+  lightboxImage.setAttribute('alt', '');
+
+  window.removeEventListener('keydown', onEscPress);
+}
+
+// --- click on overlay ---
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+lightboxOverlay.addEventListener('click', () => {
+  console.log('click on overlay');
+  closeLightbox();
+});
+
+// --- click on ESC button ---
+function onEscPress(e) {
+  // window.addEventListener('keydown', e => {
+  console.log(e);
+  if (e.code === 'Escape') {
+    closeLightbox();
+  }
+}
