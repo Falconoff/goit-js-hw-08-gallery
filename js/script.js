db8f8f45d2152ab9523f7ecd29f61bf61ffc432d
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-09-07 14:56:51 +0300

    rendreing gallery from array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+import galleryItems from '../app.js';
+// console.log(galleryItems);
+
 const galleryEl = document.querySelector('.js-gallery');
 const modal = document.querySelector('.js-lightbox');
 const closeModalBtn = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
+
+const galleryItemsArray = galleryItems.map(
+  ({ preview, original, description }) => {
+    // console.log(description);
+    return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  },
+);
+// console.log(galleryItemsArray.join(''));
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsArray.join(''));
