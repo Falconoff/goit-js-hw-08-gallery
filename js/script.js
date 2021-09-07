20f33ca450e4d92f884ef476266750259b0503f8
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-09-07 13:54:15 +0300

    copy start files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const galleryEl = document.querySelector('.js-gallery');
+const modal = document.querySelector('.js-lightbox');
+const closeModalBtn = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
